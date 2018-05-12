1c4d2cd11a09147166d91423f9598aefb0425282
andrew.bandura@gmail.com <a1n9d7y7>
2018-05-13 01:50:03 +0300

    -index controller added -refactored

--- src/main/java/com/andy/airline/controller/IndexController.java
+++ src/main/java/com/andy/airline/controller/IndexController.java
@@ -15,7 +15,7 @@ public class IndexController {
     }
     @RequestMapping(method = RequestMethod.GET)
     String getIndexPage1(){
-        return "index.html";
+        return "index";
 
     }
 
